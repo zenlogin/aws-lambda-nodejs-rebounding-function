8eb7796c082c48ac4bd866e9bfea28c13d4d3904
Oliver Nassar <github@olivernassar.com>
2024-05-14 19:05:22 -0400

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -2,31 +2,64 @@ import https from 'https';
 var __modules = {};
 __modules.https = https;
 export const handler = async function(event) {
-    var request = function(url) {
+
+    // Edit this array if you want other event names rebounded
+    // See: https://www.gorillastack.com/blog/real-time-events/cloudtrail-event-names/
+    var allowableEventNames = [
+            'ConsoleLogin'
+        ],
+        eventName = event.detail.eventName,
+        request = function(url) {
             var promise = new Promise(function(resolve, reject) {
-                var options = {
-                        method: 'GET'
+
+                // Function fails
+                if (allowableEventNames.includes(eventName) === false) {
+                    reject('Invalid eventName: ' + (eventName));
+                    return false;
+                }
+                var data = {};
+                data.event = event;
+                var postData = JSON.stringify(data),
+                    options = {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Content-Length': postData.length
+                        }
                     },
-                    req = __modules.https.request(url, options, function(response) {
-                        var data = '';
-                        response.on('data', function(chunk) {
-                            data += chunk;
+                    req = https.request(url, options, function(res) {
+                        var body = '';
+                        res.on('data', function(chunk) {
+                            body += chunk.toString();
                         });
-                        response.on('end', function() {
-                            resolve(data);
+                        res.on('end', function() {
+                            var statusCode = res.statusCode;
+                            if (statusCode === 200) {
+
+                                // Function succeeds
+                                var response = JSON.parse(body);
+                                resolve('Successful rebound');
+                            } else {
+
+                                // Function fails
+                                reject('Invalid status code: ' + (statusCode));
+                            }
                         });
-                    }).on('error', function(err) {
-                        reject(err)
+                        res.on('error', reject);
                     });
+                req.on('error', reject);
+                req.write(postData);
                 req.end();
             });
             return promise;
         },
-        url = 'https://416.io/utils/ping?abc=123';
-    await request(url);
+
+        // Replace this URL with your preferred endpoint
+        url = 'https://416.io/utils/ping';
+    var msg = await request(url);
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify('Webhook received successfully!'),
     };
     return response;
 };
